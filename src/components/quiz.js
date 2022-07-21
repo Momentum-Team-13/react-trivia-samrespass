d3693a6d0f2ad09df78e48ff876d1c84528b9e20
samrespass <samrespass@gmail.com>
2022-07-21 14:26:03 -0400

    oof

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -2,32 +2,40 @@ import '../App.css';
 import React,{useState,useEffect} from 'react'
 import axios from "axios"
 
-export default function Quizzer ({category}) {
+function Quizzer ({category}) {
     const [round, setRound] = useState([]);
-    const [loading2, setLoading2] = useState(true);
-    const [slide, setSlide] = useState([0]);
-    const [points, setPoints] = useState([0])
-
+    const [slide, setSlide] = useState(0);
+    const [points, setPoints] = useState(0)
+    const [shuffle, setShuffle] = useState([20, 40, 60, 80])
+ 
     useEffect(() => {
         axios
         .get(`https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple&category=${category}`)
-        .then((response) => setRound(response.data.results),setLoading2(false),console.log("why god"))
+        .then((response) => setRound(response.data.results),)
         }, [category])
-    if (loading2 === true) return "Loading..."
+    if (round.length === 0) {
+        return (
+            <div className="quiz">
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
     if (round.length > 0) {
             return (
                <div className="quiz">
                <div className="question"><h1>{round[slide].question}</h1>
+               
                <div className="answers">  {round[slide].incorrect_answers.map(answer => (
-           
-           <button className="answer" onClick={() => {setPoints(points + 10);setSlide(slide + 1)}}>{answer}</button>
+          
+           <button className="answer" onClick={() => {setPoints(points + 10);setSlide(slide + 1);console.log(slide)}}>{answer}</button>
   
-          ))} <button className="answer" onClick={() => {setPoints(points - 10);setSlide(slide + 1)}}>{round[slide].correct_answer}</button></div></div>
+          ))} <button className="answer" onClick={() => {setPoints(points - 10);setSlide(slide + 1);console.log(slide)}}>{round[slide].correct_answer}</button></div></div>
       
                  
                </div> 
                       
-           
              )
         }
 }
+
+export default Quizzer
--- src/components/quizboard.js
+++ src/components/quizboard.js
@@ -6,21 +6,18 @@ import Quiz from './quiz.js'
 
 export default function Quizboard () {
     const [topic, setTopic] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [quiz, setQuiz] = useState(false);
-    const [category, setCategory] = useState([0])
+    const [category, setCategory] = useState(0)
     
 
     useEffect(() => {
         axios
         .get("https://opentdb.com/api_category.php")
         .then((response) => setTopic(response.data.trivia_categories))
-        .then(setLoading(false))
         .catch(err=>console.log(err))
         }, [])
-        
-        if (loading === true) {return ("Loading...")}
-        else {
+
+        if (topic.length > 0) {
             return (
                <div className="quizBoard">
                 {quiz === true && <button className="cancelQuiz" onClick={() => setQuiz(false)}>Quit</button>}
